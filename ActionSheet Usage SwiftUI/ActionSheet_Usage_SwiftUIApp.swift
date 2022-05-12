b27e158eb0b5d86a8de97cedcefe5ca43c28f326
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-12 12:36:08 +0300

    Initial Commit

---
+++ ActionSheet Usage SwiftUI/ActionSheet_Usage_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionSheet_Usage_SwiftUIApp.swift
+//  ActionSheet Usage SwiftUI
+//
+//  Created by Ömer Faruk Kılıçaslan on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ActionSheet_Usage_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
